527d648d47eded1166ad37c3eaba10eb39a66efb
Vitaly Gashkov <vitalygashkov@vk.com>
2024-03-23 22:13:38 +0500

    feat: use mpd-parser

--- dasha.js
+++ dasha.js
@@ -1,4 +1,141 @@
-module.exports = {
-  ...require('./lib/manifest'),
-  ...require('./lib/constants'),
+'use strict';
+
+const { inheritAttributes, stringToMpdXml, toPlaylists, toM3u8 } = require('mpd-parser');
+
+const parse = (text, url, eventHandler) => {
+  const options = { manifestUri: url, eventHandler };
+  const parsedManifestInfo = inheritAttributes(stringToMpdXml(text), options);
+  const playlists = toPlaylists(parsedManifestInfo.representationInfo);
+  const manifest = toM3u8({
+    dashPlaylists: playlists,
+    locations: parsedManifestInfo.locations,
+    contentSteering: parsedManifestInfo.contentSteeringInfo,
+    eventStream: parsedManifestInfo.eventStream,
+  });
+  manifest.allPlaylists = playlists;
+  return manifest;
 };
+
+const getBestPlaylist = (playlists) => {
+  const maxBandwidth = Math.max(...playlists.map((playlist) => playlist.attributes.BANDWIDTH));
+  return playlists.find((playlist) => playlist.attributes.BANDWIDTH === maxBandwidth);
+};
+
+const getPlaylistPssh = (playlist) => {
+  const pssh = playlist.contentProtection?.['com.widevine.alpha']?.pssh;
+  return pssh ? Buffer.from(pssh).toString('base64') : null;
+};
+
+const getPssh = (manifest) => {
+  const playlist = getBestPlaylist(manifest.playlists);
+  return getPlaylistPssh(playlist);
+};
+
+const getSegments = (playlist) => {
+  const segments =
+    playlist.segments.map((segment) => ({
+      url: segment.resolvedUri,
+    })) || [];
+  if (playlist.segments.length && playlist.segments[0].map?.resolvedUri)
+    segments.unshift({ url: playlist.segments[0].map.resolvedUri, init: true });
+  return segments;
+};
+
+const qualities = [
+  { width: 7680, height: 4320 },
+  { width: 3840, height: 2160 },
+  { width: 2560, height: 1440 },
+  { width: 1920, height: 1080 },
+  { width: 1280, height: 720 },
+  { width: 854, height: 480 },
+  { width: 640, height: 360 },
+  { width: 426, height: 240 },
+  { width: 256, height: 144 },
+];
+const getWidth = (height) => qualities.find((q) => q.height === height)?.width;
+const getHeight = (width) => qualities.find((q) => q.width === width)?.height;
+
+const getVideoTrack = (manifest, height) => {
+  const matchHeight = (playlist) => {
+    if (!height) return false;
+    const playlistWidth = playlist.attributes.RESOLUTION.width;
+    const targetWidth = getWidth(height);
+    if (!playlistWidth || !targetWidth) return false;
+    return playlistWidth === targetWidth;
+  };
+
+  const playlist = manifest.playlists.find(matchHeight) || getBestPlaylist(manifest.playlists);
+  const segments = getSegments(playlist);
+  const qualityLabel = `${
+    getHeight(playlist.attributes.RESOLUTION.width) || playlist.attributes.RESOLUTION.height
+  }p`;
+
+  return {
+    type: 'video',
+    id: 0,
+    segments: segments,
+    codecs: playlist.attributes.CODECS,
+    bitrate: Math.round(playlist.attributes.BANDWIDTH / 1024), // KiB/s
+    size: Math.ceil((playlist.attributes.BANDWIDTH / 8e6) * manifest.duration), // MB
+    pssh: getPlaylistPssh(playlist),
+    licenseUrl: playlist.contentSteering?.serverURL || null,
+    width: playlist.attributes.RESOLUTION.width,
+    height: playlist.attributes.RESOLUTION.height,
+    quality: qualityLabel,
+    qualityLabel: qualityLabel,
+  };
+};
+
+const getAudioTracks = (manifest, languages = []) => {
+  const audioCollection = manifest.mediaGroups.AUDIO.audio;
+  if (!audioCollection) return [];
+  const audios = Object.values(audioCollection).filter((audio) => {
+    if (languages.length) languages.some((lang) => audio.language.startsWith(lang));
+    else return true;
+  });
+
+  return audios.map((audio, index) => {
+    const playlist = getBestPlaylist(audio.playlists);
+    const segments = getSegments(playlist);
+    const label = Object.entries(audioCollection).find(([_, value]) => value === audio)?.[0];
+    return {
+      type: 'audio',
+      id: index,
+      label,
+      segments,
+      bitrate: Math.round(playlist.attributes.BANDWIDTH / 1024), // KiB/s
+      size: Math.ceil((playlist.attributes.BANDWIDTH / 8e6) * manifest.duration), // MB
+      pssh: getPlaylistPssh(playlist),
+      licenseUrl: playlist.contentSteering?.serverURL || null,
+      audioSampleRate: 0,
+      language: audio.language,
+    };
+  });
+};
+
+const getSubtitleTracks = (manifest, languages = []) => {
+  const subCollection = manifest.mediaGroups.SUBTITLES.subs;
+  if (!subCollection) return [];
+  const subs = Object.values(subCollection).filter((sub) => {
+    if (languages.length) languages.some((lang) => sub.language.startsWith(lang));
+    else return true;
+  });
+  return subs.map((sub, index) => {
+    const playlist = getBestPlaylist(sub.playlists);
+    const segments = getSegments(playlist);
+    const label = Object.entries(subCollection).find(([_, value]) => value === sub)?.[0];
+    const originalPlaylist = manifest.allPlaylists.find((p) => p.attributes.label === label);
+    const language = originalPlaylist?.attributes.lang || sub.language;
+    return {
+      type: 'text',
+      id: index,
+      label,
+      segments,
+      language,
+      bitrate: Math.round(playlist.attributes.BANDWIDTH / 1024), // KiB/s
+      size: Math.ceil((playlist.attributes.BANDWIDTH / 8e6) * manifest.duration), // MB
+    };
+  });
+};
+
+module.exports = { parse, getPssh, getVideoTrack, getAudioTracks, getSubtitleTracks };
--- types/dasha.d.ts
+++ types/dasha.d.ts
@@ -1,2 +1,121 @@
 export * from './manifest';
 export * from './constants';
+
+export type ParseEventHandler = (event: { type: string; message: string }) => void;
+
+export interface Manifest {
+  uri: string;
+  resolvedUri: string;
+  duration: number;
+  targetDuration: number;
+  discontinuityStarts: number[];
+  timelineStarts: { start: number; timeline: number }[];
+  timeline: number;
+  mediaSequence: number;
+  discontinuitySequence: number;
+  allowCache: boolean;
+  endList: boolean;
+  segments: Segment[];
+  playlists: Playlist[];
+  mediaGroups: MediaGroups;
+
+  allPlaylists: Playlist[];
+
+  // TODO: Check types below
+  contentSteering?: {
+    defaultServiceLocation: string;
+    proxyServerURL: string;
+    queryBeforeStart: boolean;
+    serverURL: string;
+  };
+  playlistType?: string;
+  dateTimeString?: string;
+  dateTimeObject?: Date;
+  totalDuration?: number;
+}
+
+export interface Segment {
+  number: number;
+  uri: string;
+  resolvedUri: string;
+  timeline: number;
+  duration: number;
+  presentationTime: number;
+  map: {
+    uri: string;
+    resolvedUri: string;
+    // TODO: Check types below
+    byterange?: {
+      length: number;
+      offset: number;
+    };
+  };
+
+  // TODO: Check types below
+  byterange?: {
+    length: number;
+    offset: number;
+  };
+  discontinuity?: number;
+  key?: {
+    method: string;
+    uri: string;
+    iv: string;
+  };
+  'cue-out'?: string;
+  'cue-out-cont'?: string;
+  'cue-in'?: string;
+}
+
+export interface Playlist extends Omit<Manifest, 'playlists' | 'mediaGroups'> {
+  attributes: Attributes;
+  contentProtection?: {
+    mp4protection?: {
+      attributes: { schemeIdUri: string; value: string; 'cenc:default_KID': string };
+    };
+    'com.microsoft.playready'?: {
+      attributes: { schemeIdUri: string; value: string };
+      pssh: Uint8Array;
+    };
+    'com.widevine.alpha'?: {
+      attributes: { schemeIdUri: string };
+      pssh: Uint8Array;
+    };
+  };
+}
+
+export interface MediaGroups {
+  AUDIO: {
+    audio?: { [groupId: string]: MediaGroup };
+  };
+  SUBTITLES: {
+    subs?: { [groupId: string]: MediaGroup };
+  };
+  VIDEO: {};
+  'CLOSED-CAPTIONS': {};
+}
+
+export interface MediaGroup {
+  language: string;
+  default: boolean;
+  autoselect: boolean;
+  playlists: Playlist[];
+  uri: string;
+
+  // TODO: Check types below
+  instreamId?: string;
+  characteristics?: string;
+  forced?: boolean;
+}
+
+export interface Attributes {
+  NAME: string;
+  AUDIO: string;
+  SUBTITLES: string;
+  RESOLUTION: { width: number; height: number };
+  CODECS: string;
+  BANDWIDTH: number;
+  'PROGRAM-ID': number;
+  'FRAME-RATE': number;
+  [key: string]: any;
+}
