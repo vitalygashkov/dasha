9ef429a1d0fd8e03e8fa5be431bcc85eee6de347
Vitaly Gashkov <vitalygashkov@vk.com>
2024-04-06 21:30:54 +0500

    test: use kion url

--- test/dasha.test.js
+++ test/dasha.test.js
@@ -10,11 +10,13 @@ test('getQualityLabel', () => {
   strictEqual(getQualityLabel({ width: 1920, height: 1080 }), '1080p');
 });
 
+const kionUrl =
+  'https://offload.kion.ru/htv-rrs.mts.ru/88888888/16/20230707/268697239/268697239.mpd';
+
 test('DASH parsing', async () => {
-  const url =
-    'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd';
+  const url = kionUrl;
   const response = await fetch(url);
   const text = await response.text();
   const manifest = await parse(text, url);
-  strictEqual(manifest.tracks.all.length, 7);
+  strictEqual(manifest.tracks.all.length, 5);
 });
