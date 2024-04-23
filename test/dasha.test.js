0f1b8c1260150a827b314680fccf46d0f76954cb
Vitaly Gashkov <vitalygashkov@vk.com>
2024-04-23 10:38:33 +0500

    fix: parse base url without manifest url

--- test/dasha.test.js
+++ test/dasha.test.js
@@ -6,19 +6,26 @@ const { parse } = require('../dasha');
 
 const load = (path) => readFileSync(path, 'utf8');
 
-test('getQualityLabel', () => {
-  strictEqual(getQualityLabel({ width: 1920, height: 1080 }), '1080p');
+test('get video quality label', () => {
+  strictEqual(getQualityLabel({ width: 1920, height: 804 }), '1080p');
 });
 
 const kionUrl =
   'https://offload.kion.ru/htv-rrs.mts.ru/88888888/16/20230707/268697239/268697239.mpd';
-const kionUrl2 =
-  'https://offload.kion.ru/htv-rrs.mts.ru/88888888/16/20240401/268725201/268725201.mpd';
 
-test('DASH parsing', async () => {
+test('DASH: parse with URL parameter', async () => {
   const url = kionUrl;
   const response = await fetch(url);
   const text = await response.text();
   const manifest = await parse(text, url);
   strictEqual(manifest.tracks.all.length, 5);
+});
+
+test('DASH: parse without URL parameter', async () => {
+  const text = load('./test/cr.mpd');
+  const manifest = await parse(text);
+  strictEqual(manifest.tracks.all.length, 8);
+  const initUrl =
+    'https://a-vrv.akamaized.net/evs3/8a1b3acce53d49eea0ce2104fae30046/assets/p/c46e06c5fd496e8aec0b6776b97eca3f_,3748583.mp4,3748584.mp4,3748582.mp4,3748580.mp4,3748581.mp4,.urlset/init-f1-v1-x3.mp4?t=exp=1713871228~acl=/evs3/8a1b3acce53d49eea0ce2104fae30046/assets/p/c46e06c5fd496e8aec0b6776b97eca3f_,3748583.mp4,3748584.mp4,3748582.mp4,3748580.mp4,3748581.mp4,.urlset/*~hmac=7dc7daeb338da040c65111a88cbf947505a5897f42d1c433c3858f8d890ed29c';
+  strictEqual(manifest.tracks.videos[0].segments[0].url, initUrl);
 });
